c8133f2efb996c772df4b93f098700dc21ac3fd0
Ilya Shpigor <petrsum@gmail.com>
2018-07-04 12:15:25 +0200

    Get the unit type in the units.lua module

--- bots/utility/units.lua
+++ bots/utility/units.lua
@@ -11,13 +11,36 @@ local function DoWithKeysAndElements(list, do_function)
   end
 end
 
+local UNIT_TYPE = {
+  CREEP = {},
+  HERO = {},
+  TOWER = {},
+  UNDEFINED = {}
+}
+
+local function GetUnitType(unit)
+  if (unit:IsCreep()) then
+    return UNIT_TYPE["CREEP"]
+  end
+  if (unit:IsHero()) then
+    return UNIT_TYPE["HERO"]
+  end
+  if (unit:IsTower()) then
+    return UNIT_TYPE["TOWER"]
+  end
+
+  return UNIT_TYPE["UNDEFINED"]
+end
+
 local function AddToUnitList(_, unit)
   UNIT_LIST[GetTeam()][tostring(unit)] = {
     name = unit:GetUnitName(),
     location = unit:GetLocation(),
     health = unit:GetHealth(),
     mana = unit:GetMana(),
-    is_alive = unit:IsAlive()
+    is_alive = unit:IsAlive(),
+    type = GetUnitType(unit),
+    attack_target = unit:GetTarget(),
   }
 end
 
