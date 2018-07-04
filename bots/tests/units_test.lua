44ee4d28194eb26279d73e5618a61996e1e09b2d
Ilya Shpigor <petrsum@gmail.com>
2018-07-04 12:16:58 +0200

    Initial add tests for the units module

--- bots/tests/units_test.lua
+++ bots/tests/units_test.lua
@@ -6,4 +6,8 @@ require("global_functions")
 local units = require("units")
 local luaunit = require("luaunit")
 
+function test_Process_first_objective_succeed()
+  units.UpdateUnitList()
+end
+
 os.exit(luaunit.LuaUnit.run())
