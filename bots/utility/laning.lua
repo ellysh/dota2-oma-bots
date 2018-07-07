415cc1adc994818c4fbaa5c016158c83a8eba27f
Ilya Shpigor <petrsum@gmail.com>
2018-07-07 22:18:29 +0200

    Consider the incoming damage to a unit when last hitting and denying

--- bots/utility/laning.lua
+++ bots/utility/laning.lua
@@ -87,8 +87,41 @@ local SIDE = {
   ALLY = {},
 }
 
+local function GetTotalDamageToUnit(unit_data)
+  local result = 0
+
+  local unit_list = common_algorithms.GetEnemyCreeps(
+                         unit_data,
+                         constants.MAX_CREEP_ATTACK_RANGE)
+
+  result = result + common_algorithms.GetTotalDamage(unit_list, unit_data)
+
+  unit_list = common_algorithms.GetEnemyBuildings(
+                         unit_data,
+                         constants.MAX_TOWER_ATTACK_RANGE)
+
+  result = result + common_algorithms.GetTotalDamage(unit_list, unit_data)
+
+  unit_list = common_algorithms.GetEnemyHeroes(
+                         unit_data,
+                         constants.MAX_HERO_ATTACK_RANGE)
+
+  result = result + common_algorithms.GetTotalDamage(unit_list, unit_data)
+
+  unit_list = common_algorithms.GetAllyHeroes(
+                         unit_data,
+                         constants.MAX_HERO_ATTACK_RANGE)
+
+  result = result + common_algorithms.GetTotalDamage(unit_list, unit_data)
+
+  return result
+end
+
 local function IsLastHit(bot_data, unit_data)
-  return unit_data.health <= bot_data.attack_damage
+  local incoming_damage = bot_data.attack_damage
+                          + GetTotalDamageToUnit(unit_data)
+
+  return unit_data.health <= incoming_damage
 end
 
 local function GetLastHitCreep(bot_data, side)
