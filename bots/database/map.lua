621316184a92921d253a0f792e3343a12fb2b607
Ilya Shpigor <petrsum@gmail.com>
2018-07-20 22:11:20 +0200

    Fix format of the MAP table

--- bots/database/map.lua
+++ bots/database/map.lua
@@ -2,80 +2,24 @@
 local M = {}
 
 M.MAP = {
-  {
-    spot = "tower_tier_1",
-    location = Vector(-1563, -1408, 144),
-    team = TEAM_RADIANT,
+  [TEAM_RADIANT] = {
+    tower_tier_1_attack = Vector(-1563, -1408, 710),
+    tower_tier_1_day_vision = Vector(-1563, -1408, 1900),
+    tower_tier_1_night_vision = Vector(-1563, -1408, 800),
+    high_ground = Vector(-1073, 952, 350),
+    forest_bot = Vector(-908, -1541, 140),
+    forest_top = Vector(-2045, -1000, 100),
   },
-  {
-    spot = "tower_tier_1_attack",
-    location = Vector(-1563, -1408, 700),
-    team = TEAM_RADIANT,
+  [TEAM_DIRE] = {
+    tower_tier_1_attack = Vector(531, 653, 710),
+    tower_tier_1_day_vision = Vector(531, 653, 1900),
+    tower_tier_1_night_vision = Vector(531, 653, 800),
+    high_ground = Vector(46, 155, 350),
+    forest_bot = Vector(1024, 124, 70),
+    forest_top = Vector(550, 1165, 100),
   },
-  {
-    spot = "tower_tier_1_day_vision",
-    location = Vector(-1563, -1408, 1900),
-    team = TEAM_RADIANT,
-  },
-  {
-    spot = "tower_tier_1_night_vision",
-    location = Vector(-1563, -1408, 800),
-    team = TEAM_RADIANT,
-  },
-  {
-    spot = "tower_tier_1",
-    location = Vector(531, 653, 144),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "tower_tier_1_attack",
-    location = Vector(531, 653, 700),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "tower_tier_1_day_vision",
-    location = Vector(531, 653, 1900),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "tower_tier_1_night_vision",
-    location = Vector(531, 653, 800),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "high_ground",
-    location = Vector(-1073, 952, 350),
-    team = TEAM_RADIANT,
-  },
-  {
-    spot = "high_ground",
-    location = Vector(46, 155, 350),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "river",
-    location = Vector(-521, -375, 280),
-    team = TEAM_NEUTRAL,
-  },
-  {
-    spot = "forest_bot",
-    location = Vector(-908, -1541, 140),
-    team = TEAM_RADIANT,
-  },
-  {
-    spot = "forest_top",
-    location = Vector(-2045, -1000, 100),
-    team = TEAM_RADIANT,
-  },
-  {
-    spot = "forest_bot",
-    location = Vector(1024, 124, 70),
-    team = TEAM_DIRE,
-  },
-  {
-    spot = "forest_top",
-    location = Vector(550, 1165, 100),
-    team = TEAM_DIRE,
+  [TEAM_NEUTRAL] = {
+    river = Vector(-521, -375, 280),
   },
 }
 
