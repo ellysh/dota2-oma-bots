ebc17abe2b21b05662a8d4b2f4b5569e7766b1e6
Ilya Shpigor <petrsum@gmail.com>
2018-07-22 11:27:25 +0200

    Add the SKILL_BUILD table

---
+++ bots/database/skill_build.lua
@@ -0,0 +1,26 @@
+
+local M = {}
+
+M.SKILL_BUILD = {
+  [1] = "drow_ranger_trueshot",
+  [2] = "drow_ranger_frost_arrows",
+  [3] = "drow_ranger_trueshot",
+  [4] = "drow_ranger_frost_arrows",
+  [5] = "drow_ranger_wave_of_silence",
+  [6] = "drow_ranger_marksmanship",
+  [7] = "drow_ranger_trueshot",
+  [8] = "drow_ranger_trueshot",
+  [9] = "drow_ranger_frost_arrows",
+  [10] = "special_bonus_all_stats_5",
+  [11] = "drow_ranger_frost_arrows",
+  [12] = "drow_ranger_marksmanship",
+  [13] = "drow_ranger_wave_of_silence",
+  [14] = "drow_ranger_wave_of_silence",
+  [15] = "special_bonus_attack_speed_20",
+  [16] = "drow_ranger_wave_of_silence",
+  [18] = "drow_ranger_marksmanship",
+  [20] = "special_bonus_unique_drow_ranger_1",
+  [25] = "special_bonus_unique_drow_ranger_3",
+}
+
+return M
