c497a6c6ed18d1aec1bdf50a8019ae8d42cd56cb
Ilya Shpigor <petrsum@gmail.com>
2018-07-22 09:07:52 +0200

    Clarify coordinates of the towers

--- bots/database/map.lua
+++ bots/database/map.lua
@@ -3,18 +3,18 @@ local M = {}
 
 M.MAP = {
   [TEAM_RADIANT] = {
-    tower_tier_1_attack = Vector(-1563, -1408, 710),
-    tower_tier_1_day_vision = Vector(-1563, -1408, 1900),
-    tower_tier_1_night_vision = Vector(-1563, -1408, 800),
+    tower_tier_1_attack = Vector(-1544, -1408, 710),
+    tower_tier_1_day_vision = Vector(-1544, -1408, 1900),
+    tower_tier_1_night_vision = Vector(-1544, -1408, 800),
     high_ground = Vector(-1073, -952, 350),
     forest_bot = Vector(-908, -1541, 140),
     forest_top = Vector(-2045, -1000, 100),
     fountain = Vector(-7000, -6500, 500),
   },
   [TEAM_DIRE] = {
-    tower_tier_1_attack = Vector(531, 653, 710),
-    tower_tier_1_day_vision = Vector(531, 653, 1900),
-    tower_tier_1_night_vision = Vector(531, 653, 800),
+    tower_tier_1_attack = Vector(524, 652, 710),
+    tower_tier_1_day_vision = Vector(524, 652, 1900),
+    tower_tier_1_night_vision = Vector(524, 652, 800),
     high_ground = Vector(46, 155, 350),
     forest_bot = Vector(1024, 124, 70),
     forest_top = Vector(550, 1165, 100),
