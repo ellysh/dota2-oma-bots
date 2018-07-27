1cbef675240cc240da4d2fbbc9b7446df4dd5fab
Ilya Shpigor <petrsum@gmail.com>
2018-07-27 20:20:50 +0200

    Increase delay after the evasion move

--- bots/utility/laning.lua
+++ bots/utility/laning.lua
@@ -333,7 +333,7 @@ end
 function M.evasion()
   BOT:Action_MoveToLocation(map.GetAllySpot(BOT_DATA, "fountain"))
 
-  action_timing.SetNextActionDelay(0.8)
+  action_timing.SetNextActionDelay(1.6)
 end
 
 --------------------------------
