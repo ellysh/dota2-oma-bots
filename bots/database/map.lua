7dcfc0eecf231772e4c64c0842dee2f27f7ced57
Ilya Shpigor <petrsum@gmail.com>
2018-08-12 09:45:07 +0200

    Increase a tower attack range to avoid mistakes

--- bots/database/map.lua
+++ bots/database/map.lua
@@ -3,7 +3,7 @@ local M = {}
 
 M.MAP = {
   [TEAM_RADIANT] = {
-    tower_tier_1_attack = Vector(-1544, -1408, 710),
+    tower_tier_1_attack = Vector(-1544, -1408, 800),
     tower_tier_1_day_vision = Vector(-1544, -1408, 1900),
     tower_tier_1_night_vision = Vector(-1544, -1408, 800),
     high_ground = Vector(-1073, -952, 350),
@@ -14,7 +14,7 @@ M.MAP = {
     tp_tower_tier_1 = Vector(-1181, -1741, 100),
   },
   [TEAM_DIRE] = {
-    tower_tier_1_attack = Vector(524, 652, 710),
+    tower_tier_1_attack = Vector(524, 652, 800),
     tower_tier_1_day_vision = Vector(524, 652, 1900),
     tower_tier_1_night_vision = Vector(524, 652, 800),
     high_ground = Vector(46, 155, 350),
