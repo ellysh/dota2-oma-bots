ea096be91b06885a2639b1967c2415ed27b31cba
Ilya Shpigor <petrsum@gmail.com>
2018-08-16 19:05:05 +0200

    Add team information when printing bots version to console

--- bots/utility/logger.lua
+++ bots/utility/logger.lua
@@ -1,9 +1,13 @@
+local constants = require(
+  GetScriptDirectory() .."/utility/constants")
+
 local M = {}
 
 function M.Print(string)
   print(GameTime() .. ": " ..string .. "\n")
 end
 
-M.Print("OMA Bots version 0.9")
+M.Print("OMA Bots version " .. constants.BOTS_VERSION ..
+        " play in team " .. GetTeam())
 
 return M
