ce49adb0bac4b04818c1ccd35322c646db97617a
Ilya Shpigor <petrsum@gmail.com>
2018-08-24 22:07:08 +0200

    Remove the broken unit tests

--- bots/tests/objectives_test.lua
+++ bots/tests/objectives_test.lua
@@ -6,19 +6,4 @@ require("global_functions")
 local objectives = require("objectives")
 local luaunit = require("luaunit")
 
-function test_Process_first_objective_succeed()
-  TIME = -1
-  IS_COURIER_AVAILABLE = false
-  objectives.Process()
-
-  -- We need the second Process call to update the courier state
-  IS_COURIER_AVAILABLE = true
-  objectives.Process()
-end
-
-function test_Process_second_objective_succeed()
-  IS_COURIER_AVAILABLE = true
-  objectives.Process()
-end
-
 os.exit(luaunit.LuaUnit.run())
