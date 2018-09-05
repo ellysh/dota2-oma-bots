86d688e0de40b99cb001ccf8a8f834dca3d37282
Ilya Shpigor <petrsum@gmail.com>
2018-09-05 21:07:36 +0200

    Add support of several moves in one objective for the generator script

--- generator/generator.py
+++ generator/generator.py
@@ -30,37 +30,62 @@ def get_value(line, index):
   value = line[index].strip().replace("\n"," ")
   return value if value else 'nil'
 
-def get_actions(reader):
-  for line in reader:
-    if line[4]:
-      action = t.ACTION
-      action = action.replace('<' + str(4) + '>', get_value(line, 4))
-      return action
+def print_objective(line):
+  objective = t.OBJECTIVE_HEADER
+  objective = objective.replace('<' + str(0) + '>', get_value(line, 0))
+  objective = objective.replace('<' + str(1) + '>', get_value(line, 1))
+  sys.stdout.write(objective)
+  return
 
-def get_moves(reader):
-  for line in reader:
-    if line[2]:
-      move = t.MOVE
-      move = move.replace('<' + str(2) + '>', get_value(line, 2))
-      move = move.replace('<' + str(3) + '>', get_value(line, 3))
-      actions = get_actions(reader)
-      move = move.replace('<ACTIONS>', actions)
-      return move
+def print_move(line):
+  move = t.MOVE_HEADER
+  move = move.replace('<' + str(2) + '>', get_value(line, 2))
+  move = move.replace('<' + str(3) + '>', get_value(line, 3))
+  sys.stdout.write(move)
+  return
 
-def print_objective(line, reader):
+def print_action(line):
+  action = t.ACTION
+  action = action.replace('<' + str(4) + '>', get_value(line, 4))
+  sys.stdout.write(action)
+  return
+
+IS_FIRST_OBJECTIVE = True
+IS_FIRST_MOVE = True
+
+def print_element(line):
+  global IS_FIRST_OBJECTIVE
+  global IS_FIRST_MOVE
+
   if line[0]:
-    objective = t.OBJECTIVE
-    objective = objective.replace('<' + str(0) + '>', get_value(line, 0))
-    objective = objective.replace('<' + str(1) + '>', get_value(line, 1))
-    moves = get_moves(reader)
-    objective = objective.replace('<MOVES>', moves)
-    sys.stdout.write(objective)
+    if not IS_FIRST_OBJECTIVE:
+      sys.stdout.write(t.MOVE_FOOTER)
+      sys.stdout.write(t.OBJECTIVE_FOOTER)
+
+    IS_FIRST_OBJECTIVE = False
+    IS_FIRST_MOVE = True
+    print_objective(line)
+    return
+
+  if line[2]:
+    if not IS_FIRST_MOVE:
+      sys.stdout.write(t.MOVE_FOOTER)
+
+    IS_FIRST_MOVE = False
+    print_move(line)
+    return
+
+  if line[4]:
+    print_action(line)
 
 def parse_lines(reader):
   skip_header_lines(reader)
 
   for line in reader:
-    print_objective(line, reader)
+    print_element(line)
+
+  sys.stdout.write(t.MOVE_FOOTER)
+  sys.stdout.write(t.OBJECTIVE_FOOTER)
 
 def parse_csv_file(filename):
   with open(filename, "rU") as file_obj:
--- generator/templates.py
+++ generator/templates.py
@@ -12,27 +12,32 @@ return M
 
 #---------------------------------------------
 
-OBJECTIVE = """
+OBJECTIVE_HEADER = """
   {
     objective = "<0>",
     module = require(GetScriptDirectory() .."/utility/<0>"),
     is_interruptible = <1>,
-    moves = {
-<MOVES>
+    moves = {"""
+
+OBJECTIVE_FOOTER = """
     },
   },
 """
 
 #---------------------------------------------
 
-MOVE = """      {
+MOVE_HEADER = """
+      {
         move = "<2>",
         is_interruptible = <3>,
         actions = {
-<ACTIONS>
+"""
+
+MOVE_FOOTER = """
         },
       },"""
 
 #---------------------------------------------
 
-ACTION = """          {action = "<4>"},"""
+ACTION = """          {action = "<4>"},
+"""
