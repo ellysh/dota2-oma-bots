6eafd8f1e1835d28292379fd7a9e989cebdc7b1a
Ilya Shpigor <petrsum@gmail.com>
2018-10-03 15:33:03 +0200

    Do not move to the safe spot when recovery with tango

--- bots/utility/evasion.lua
+++ bots/utility/evasion.lua
@@ -56,13 +56,7 @@ end
 
 function M.pre_move_safe_recovery()
   return (env.BOT_DATA.is_flask_healing
-           and env.BOT_DATA.health ~= env.BOT_DATA.max_health)
-
-         or (env.BOT_DATA.is_healing
-             and functions.GetRate(
-                   env.BOT_DATA.health,
-                   env.BOT_DATA.max_health)
-                 <= 0.5)
+          and env.BOT_DATA.health ~= env.BOT_DATA.max_health)
 
          and not map.IsUnitInSpot(env.BOT_DATA, env.SAFE_SPOT)
 
