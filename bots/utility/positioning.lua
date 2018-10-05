2f2db87c9d7c81e149d6377e7ca8255528ed24a8
Ilya Shpigor <petrsum@gmail.com>
2018-10-05 11:46:32 +0200

    Revert "Ignore the minimal creep distance after level 6"
    This reverts commit ca53fbdd495fa83567213d266e68f951dadd0b8b.
    
    Because of this commit, the bot comes very close to enemy creeps on
    after level 6. It happens because the decrease_creeps_distance move
    works without limitations of the increase_creeps_distance one.

--- bots/utility/positioning.lua
+++ bots/utility/positioning.lua
@@ -87,7 +87,6 @@ function M.pre_increase_creeps_distance()
                < constants.MIN_HERO_DISTANCE)
 
           or (env.ENEMY_CREEP_FRONT_DATA ~= nil
-              and not algorithms.HasLevelForAggression(env.BOT_DATA)
               and functions.GetUnitDistance(
                     env.BOT_DATA,
                     env.ENEMY_CREEP_FRONT_DATA)
