39af735671c3f6602679e231941c1e9621054d93
Ilya Shpigor <petrsum@gmail.com>
2018-12-15 22:41:45 +0100

    Revert "Do not buy the faerie fire at the start"
    This reverts commit a0e5191ffdd1e702c67d689b93cb6df62cfa4ddf.

--- bots/utility/prepare_for_match.lua
+++ bots/utility/prepare_for_match.lua
@@ -26,13 +26,14 @@ end
 function M.pre_buy_starting_items()
   return not IsGameStart()
          and not algorithms.IsItemPresent(
-                   env.BOT_DATA, "item_wraith_band")
+                   env.BOT_DATA, "item_faerie_fire")
 end
 
 function M.buy_starting_items()
   algorithms.BuyItem("item_slippers")
   algorithms.BuyItem("item_circlet")
   algorithms.BuyItem("item_recipe_wraith_band")
+  algorithms.BuyItem("item_faerie_fire")
 
   action_timing.SetNextActionDelay(0.5)
 end
